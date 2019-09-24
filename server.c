146f94c3afe4b71b31544a3bae3bdbd253aa6769
Jeff Yang <www.speedway@hotmail.com>
2019-09-24 16:36:11 -0400

    push

--- server.c
+++ server.c
@@ -10,7 +10,7 @@
 
 
 int main (int argc, char** argv){
-    int socket;
+    int sock;
     socklen_t addr_len;
     int tid_selection;
     struct sockaddr_in server;
@@ -24,8 +24,12 @@ int main (int argc, char** argv){
         printf("Number of arguments is not 3.\n");
         return 0;
     }
-    if((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+    if(sock = socket(PF_INET, SOCK_DGRAM, 0) < 0){
         printf("Could not create socket.\n");
+        return 0;
+    }
+    if(bind(sock, (struct sockaddr *) &server, sizeof(server)) < 0){
+        printf("Could not bind socket to server.\n");
         return 0;
     }
     bzero(&server, sizeof(server));
@@ -41,21 +45,34 @@ int main (int argc, char** argv){
         unsigned short* op_pointer;
         char* buffer;
         buffer = calloc(517, sizeof(char));
-        int data_size = recvfrom(sock, buffer, 1024, 0, &client, &addr_len);
+        int data_size = recvfrom(sock, buffer, 517, 0, &client, &addr_len);
         op_pointer = (unsigned short*)buffer;
         op_code = ntohs(*op_pointer);
-        else{
+        
             if(fork() == 0){
                 //child go to handle request
                 close(sock);
                 break;
             }
-        }
+            else{
+                tid_selection--;
+            }
+        
     }
-    struct sockaddr_in child_sock;
+    struct sockaddr_in child_server;
     int tid = tid_selection;
-    tid_selection--;
-    child_sock.sin_family = PF_INET;
-    child_sock.sin_port = htons(tid);
-    child_sock.sin_addr.s_addr = htonl(INADDR_ANY);
+    int child_sock;
+    //tid_selection--;
+    child_server.sin_family = PF_INET;
+    child_server.sin_port = htons(tid);
+    child_server.sin_addr.s_addr = htonl(INADDR_ANY);
+    
+    if(child_sock = socket(PF_INET, SOCK_DGRAM, 0) < 0){
+        printf("Could not create socket.\n");
+        return 0;
+    }
+    if(bind(child_sock, (struct sockaddr *) &child_sock, sizeof(child_sock)) < 0){
+        printf("Could not bind socket to server.\n");
+        return 0;
+    }
 }
